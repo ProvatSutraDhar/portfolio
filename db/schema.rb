cb0e8899e77fdb06f02f01448995bc1f9d162946
Provat SutraDhar <s.provatcsd01@gmail.com>
2022-02-13 15:21:41 +0600

    add path for ml portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_13_080633) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_13_082936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ml_portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.string "type"
+    t.string "language"
+    t.string "framework"
+    t.string "libraries"
+    t.text "description"
+    t.string "url"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
