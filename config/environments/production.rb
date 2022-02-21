27f3887a33b80079a8c50fb312220e9b7e136844
Provat SutraDhar <s.provatcsd01@gmail.com>
2022-02-22 02:46:00 +0600

    add domain in mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'https://cryptic-plains-43973.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :domain         => 'https://provatsd.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
